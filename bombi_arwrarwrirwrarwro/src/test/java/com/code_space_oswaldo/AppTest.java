8061e10c4a99f07d6479ecff968076dc1b2c8367
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-16 22:00:08 +0000

    java + maven

---
+++ bombi_arwrarwrirwrarwro/src/test/java/com/code_space_oswaldo/AppTest.java
@@ -0,0 +1,18 @@
+package com.code_space_oswaldo;
+
+//import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    //@Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.print("TESTING");
+    }
+}
