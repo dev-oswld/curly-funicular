d04df356defc525e7bdb5e1497555e25a2548958
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-16 22:30:23 +0000

    Update structure + Run tests

--- bombi_arwrarwrirwrarwro/src/main/java/com/code_space_oswaldo/App.java
+++ bombi_arwrarwrirwrarwro/src/main/java/com/code_space_oswaldo/App.java
@@ -1,9 +1,5 @@
 package com.code_space_oswaldo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
--- bombi_arwrarwrirwrarwro/src/test/java/com/code_space_oswaldo/AppTest.java
+++ bombi_arwrarwrirwrarwro/src/test/java/com/code_space_oswaldo/AppTest.java
@@ -2,9 +2,6 @@ package com.code_space_oswaldo;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
     
