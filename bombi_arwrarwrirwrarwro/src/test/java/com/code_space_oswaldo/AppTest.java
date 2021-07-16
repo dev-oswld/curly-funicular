d50e958397537aad3e4eac9d75bf2381742e03b6
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-16 22:11:03 +0000

    JUnit works!

--- bombi_arwrarwrirwrarwro/src/test/java/com/code_space_oswaldo/AppTest.java
+++ bombi_arwrarwrirwrarwro/src/test/java/com/code_space_oswaldo/AppTest.java
@@ -1,16 +1,14 @@
 package com.code_space_oswaldo;
 
-//import org.junit.Test;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    //@Test
+    
+    @Test
     public void shouldAnswerWithTrue()
     {
         System.out.print("TESTING");
