ac0a58a40ea6745679f6caf8c6b03b3bbc05182a
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-19 21:49:34 +0000

    trash

--- bombi_arwrarwrirwrarwro/src/main/java/com/code_space_oswaldo/App.java
+++
@@ -1,9 +0,0 @@
-package com.code_space_oswaldo;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World from code spaces!!!" );
-    }
-}
