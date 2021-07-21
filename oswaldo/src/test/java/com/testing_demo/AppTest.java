b6a60feee42c412de64a722fa01dec118bbc0f5a
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-21 16:10:47 +0000

    example

--- oswaldo/src/test/java/com/testing_demo/AppTest.java
+++ oswaldo/src/test/java/com/testing_demo/AppTest.java
@@ -3,11 +3,21 @@ package com.testing_demo;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AppTest 
 {
-    @Test
-   public void testAdd() {
+   @Test
+   public void driverExcercise() {
       String str = "TestNG is working fine";
       assertEquals("TestNG is working fine", str);
+
+      System .setProperty("webdriver.chrome.driver", ".\\web_drivers_general\\chromedriver.exe");
+      WebDriver driver = new ChromeDriver();
+      
+      driver.get("wwww.google.com");
+      assert driver.getTitle().startsWith("Google");
+      driver.quit();
    }
 }
