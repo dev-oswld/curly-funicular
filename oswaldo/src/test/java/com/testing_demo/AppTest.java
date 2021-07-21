736cc87814ff39c9c7042a7ea6450aff94d23b5c
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-21 17:04:00 +0000

    update path

--- oswaldo/src/test/java/com/testing_demo/AppTest.java
+++ oswaldo/src/test/java/com/testing_demo/AppTest.java
@@ -1,7 +1,7 @@
 package com.testing_demo;
 
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
+//import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,10 +10,11 @@ public class AppTest
 {
    @Test
    public void driverExcercise() {
-      String str = "TestNG is working fine";
-      assertEquals("TestNG is working fine", str);
+      //String str = "TestNG is working fine";
+      //assertEquals("TestNG is working fine", str);
 
-      System .setProperty("webdriver.chrome.driver", ".\\web_drivers_general\\chromedriver.exe");
+      ///workspaces/curly-funicular/drivers_selenium/chromedriver.exe ONLY Windows
+      System .setProperty("webdriver.chrome.driver", "/workspaces/curly-funicular/drivers_selenium/chromedriver");
       WebDriver driver = new ChromeDriver();
       
       driver.get("wwww.google.com");
