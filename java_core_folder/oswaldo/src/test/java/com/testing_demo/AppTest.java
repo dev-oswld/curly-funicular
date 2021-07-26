a021f36f90d1de21f90843b8d0c518a20f53b071
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-26 21:28:10 +0000

    Java core

---
+++ java_core_folder/oswaldo/src/test/java/com/testing_demo/AppTest.java
@@ -0,0 +1,28 @@
+package com.testing_demo;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest 
+{
+   @Test
+   public void driverExcercise() {
+      String str = "TestNG is working fine";
+      assertEquals("TestNG is working fine", str);
+
+      /* 
+      // /workspaces/curly-funicular/drivers_selenium/chromedriver.exe ONLY Windows
+      ⚠⚠⚠
+      System .setProperty("webdriver.chrome.driver", "/workspace/curly-funicular/oswaldo/drivers_/chromedriver.exe");
+      WebDriver driver = new ChromeDriver();
+      
+      driver.get("wwww.google.com");
+      assert driver.getTitle().startsWith("Google");
+      driver.quit();
+      ⚠⚠⚠
+      */
+   }
+}
