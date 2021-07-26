16f5bcb1290cef4ea90824e0f4e03d7438ef85a5
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2021-07-26 22:07:44 +0000

    Init .net project

---
+++ c_sharp_core_folder/IntroSelenium/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium.Chrome; // import this
+using System.Threading;
+
+namespace IntroSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ChromeDriver driver = new ChromeDriver();
+            driver.Url = "https://aspnetcoremaster.com";
+            Thread.Sleep(2000);
+            driver.Close();
+        }
+    }
+}
